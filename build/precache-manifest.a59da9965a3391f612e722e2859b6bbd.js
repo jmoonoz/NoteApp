6fa06a5fefb6478b94e573a28a72245364f183a3
Joel Munoz <j.munoz1@yahoo.com>
2021-12-30 18:43:53 -0800

    build created

---
+++ build/precache-manifest.a59da9965a3391f612e722e2859b6bbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc0deac4c1908e6af84c47949264c06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617db720b613fa51b745",
+    "url": "/static/css/main.b3c70823.chunk.css"
+  },
+  {
+    "revision": "bbac785755b429f47511",
+    "url": "/static/js/2.ac47d036.chunk.js"
+  },
+  {
+    "revision": "617db720b613fa51b745",
+    "url": "/static/js/main.b9da63c6.chunk.js"
+  },
+  {
+    "revision": "2f20d77946b5b4198b5c",
+    "url": "/static/js/runtime-main.b65e8437.js"
+  }
+]);
